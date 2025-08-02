643c0de38a6b2be7473113ca3480710891729073
perky-svg11 <perkywolf0107@gmail.com>
2025-08-03 00:14:40 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,21 +5,7 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {
-      // Custom class replacement for `glass-card`
-      boxShadow: {
-        'glass': '0 8px 32px 0 rgba( 31, 38, 135, 0.37 )',
-      },
-      backdropBlur: {
-        'glass': '10px',
-      },
-      backgroundColor: {
-        'glass': 'rgba(255, 255, 255, 0.1)',
-      },
-      borderColor: {
-        'glass': 'rgba(255, 255, 255, 0.18)',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
